e27b94b5be46e007486856d6968d7514f47fc897
Python <shuminnahar8di@gmail.com>
2025-11-28 11:43:05 +0600

    book appointment done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -29,6 +29,7 @@ const Profile = () => {
       .catch((error) => {
         console.log(error);
       });
+     
   };
 
   return (
--- src/pages/Servicedetails/Servicedetails.jsx
+++ src/pages/Servicedetails/Servicedetails.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router';
+import { Contextapi } from '../../Authprovider/Authprovider';
 
 
 
 const Servicedetails = () => {
     const {id}=useParams();
+    
+    const [book,setBook]=useState(false);
+
     const [services,setServices]=useState([])
             useEffect(()=>{
                 fetch('/services.json')
@@ -15,6 +19,14 @@ const Servicedetails = () => {
             },[])
 
            const findmatchid=services.find(service=>service.serviceId == id);
+
+           const handlebook=(e)=>{
+           e.preventDefault();
+           alert('Form Submitted');
+
+           e.target.reset();
+
+           }
            
     return (
         
@@ -27,10 +39,46 @@ const Servicedetails = () => {
   <div className="card-body">
     <h2 className="card-title">{findmatchid?.serviceName}</h2>
     <p>{findmatchid?.description}</p>
-    <div className="card-actions justify-end">
+    <div className="card-actions justify-between items-center">
+       <button
+            onClick={() => setBook(!book)}
+            className="mt-4 px-5 py-2 bg-green-800 text-white rounded hover:bg-green-900 transition"
+          >
+            {book ? "Close" : "Book Now"}
+          </button>
       <Link to='/services'>
       <button className="btn bg-green-900 text-white">Back</button>
       </Link>
+
+       {book && (
+        <div className="px-6 pb-6">
+          <form onSubmit={handlebook} className="space-y-4">
+            <div>
+              <label className="block mb-1 text-gray-700">Name</label>
+              <input
+                name="name"
+                type="text"
+                
+                className="w-full border rounded px-3 py-2"
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 text-gray-700">Email</label>
+              <input
+                name="photourl"
+                type="email"
+               
+                className="w-full border rounded px-3 py-2"
+              />
+            </div>
+
+            <button className="w-full py-2 bg-green-800 text-white rounded hover:bg-green-900 transition">
+             Submit
+            </button>
+          </form>
+        </div>
+      )}
       
     </div>
   </div>
